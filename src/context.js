e09f1a27bcb6bdd135855358ddd12376c39401f2
Natalie-Cardot <nataliecardot@gmail.com>
2021-07-13 22:49:41 -0700

    Set up Context

--- src/context.js
+++ src/context.js
@@ -1,1 +1,10 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+// createContext returns 2 components, provider and consumer (only using provider - consumer doesn't need to be used when using the useContext hook)
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
